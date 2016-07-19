24fba56f6b81dd36c70483a9a5c74e9242c59b01
Rachelle Herrera <rachelle.herera@gmail.com>
2016-07-19 16:54:55 -0700

    added new font to page

---
+++ js/modeling.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+  /* Side Nav Collapse */
+  $(".button-collapse").sideNav(); 
+
+  /* Slider */
+  $('.slider').slider({
+      full_width: false,
+      height:650, 
+      interval:5000, 
+      transition: 800, 
+  });
+
+  // Pause slider
+  $('.slider').slider('pause');
+  // Start slider
+  $('.slider').slider('start');
+  // Next slide
+  $('.slider').slider('next');
+  // Previous slide
+ $('.slider').slider('prev');
+
+
+
+});
